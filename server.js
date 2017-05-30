fdd6d9ebc496e3bd01734a13d1e9370485da74af
skyh <skyh@ya.ru>
2017-05-30 12:22:16 +0300

    Add index, remove auto-open

--- server.js
+++ server.js
@@ -2,11 +2,16 @@
 
 const express = require('express');
 const opn = require('opn');
+const serveIndex = require('serve-index');
 
 const backend = express();
+backend.use(serveIndex('src/pub', {icons: false, filter: (file) => /\.xml$/.test(file)}));
 backend.use(express.static('src/pub'));
 
-const server = backend.listen(8080);
+const port = 8080;
+const server = backend.listen(port);
 const app = ['chrome', '--new-window'];
 
-opn('http://localhost:8080/invoice-example.xml', {app});
+// opn('http://localhost:8080/invoice-example.xml', {app});
+
+console.log(`Open http://localhost:${port}`);
